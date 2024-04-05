6bee36c53752a49461b9259cce1843c87c9cbf63
btssio <btssio@PC-00330-EJA.cite-scolaire.lan>
2024-04-05 12:52:49 +0200

    ajout : la classe Global et connection, implémentaion Console

---
+++ ClassLibrary/ClassLibrary/Global.cs
@@ -0,0 +1,15 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    internal class Global
+    {
+        public static string pilote = "server=127.0.0.1; uid=root ; pwd=caribou; database=sio ";
+        public static MySqlConnection conn = new MySqlConnection(pilote);
+    }
+}
---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ClassLibrary;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        try
+        {
+        }
+        catch (Exception ex) { Console.WriteLine("Connexion échouée", ex.ToString()); }
+    }
+
+
+
+}
